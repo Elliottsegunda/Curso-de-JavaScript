46e3a530abe7f8f01ba65b0c1a8f2c21de0d159f
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-04 10:32:33 +0100

    Update variaveis.js

---
+++ js/variaveis.js
@@ -0,0 +1,10 @@
+/**
+ No javaScript não precisa se definir tipo de variavel, ele vai receber o seu tipo de acordo ao dados introduzidos,
+  salve-se quando se quiser especificar
+ */
+//variaveis do tipo string
+var texto = "Curso de JavaScript" 
+
+//variaveis do tipo number
+var numeroInteiro = -7
+/*Podem armazenar reais ou inteiros.*/
