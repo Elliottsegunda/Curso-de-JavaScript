037dee5305de74f569a022149b4a532e91beccf4
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-05 11:36:21 +0100

    Update ifelsecondicoes.js

--- js/ifelsecondicoes.js
+++ js/ifelsecondicoes.js
@@ -1,3 +1,9 @@
   /*
         É atraves dos operadores de comparação que nós colocamos expressoes que serão verificadas dentro dos nossos comandos if.
   */
+ if( true){
+    //caso seja verdade
+    document.write('Entrou na condição verdadeira');
+
+ }
+ //else{}
