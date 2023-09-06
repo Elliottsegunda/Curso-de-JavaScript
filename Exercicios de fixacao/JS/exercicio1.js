4f1fc407a08df9cc53baa2dbb17789422ce87257
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-06 14:03:54 +0100

    Update exercicio1.js

--- Exercicios de fixacao/JS/exercicio1.js
+++ Exercicios de fixacao/JS/exercicio1.js
@@ -4,6 +4,18 @@ var idade = prompt('Digite a sua idade');
 idade = parseInt(idade);
 
 if( idade >= 0 && idade <= 15){
-    document.write('Caro usuario, pelos criterios de idade o senhor é uma criança')
+    document.write('Caro usuario, pelos criterios de idade o senhor é uma criança');
 
+}
+else if( idade >= 15 && idade <= 30){
+    document.write('Caro usuario, pelos criterios de idade o senhor é Jovem');
+}
+else if( idade >= 30 && idade <= 60){
+    document.write('Caro usuario, pelos criterios de idade o senhor é Adulto');
+}
+else if( idade >= 60){
+    document.write('Caro usuario, pelos criterios de idade o senhor é Idoso');
+}
+else if( idade <= -1 && idade >= 108){
+    document.write('Caro usuario, pelos criterios de idade o senhor está morto');
 }
