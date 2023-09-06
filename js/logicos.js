5a0b7780c9fdbcfacd3613e733db2f2c2acdd881
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-06 13:04:50 +0100

    Update logicos.js

--- js/logicos.js
+++ js/logicos.js
@@ -1,7 +1,19 @@
 /*
- && - operador logico AND ele funciona se as 2 condições forem verdadeiras
+ && - operador logico AND, ele verifica e retorna verdadeiro  se todas as  condições forem verdadeiras
+ || - operador logico OR ele verifica e retorna verdadeiro se pelo menos uma das condições for verdadeira
+
+ if(2 == 4 || 3 >= 5 || 'a' == 'b'){
+    document.write('Verdade');
+}
+else{
+    document.write('Falso');
+}
+ele vai retornar falso porque todas as condições são falsas
 
 */
-if(2==2 && 3 >= 1){
+if(2 == 2 || 3 >= 1 && 'a' == 'b'){
     document.write('Verdade');
+}
+else{
+    document.write('Falso');
 }
