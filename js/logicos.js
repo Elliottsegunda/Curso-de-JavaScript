c8c330f810960407095e744fde7b64ddab4003bf
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-06 13:09:01 +0100

    Update logicos.js

--- js/logicos.js
+++ js/logicos.js
@@ -1,6 +1,7 @@
 /*
  && - operador logico AND, ele verifica e retorna verdadeiro  se todas as  condições forem verdadeiras
  || - operador logico OR ele verifica e retorna verdadeiro se pelo menos uma das condições for verdadeira
+ ! - operador de negação, ele inverte o resultado, se o resultado for verdadeiro ao usarmos a negação será falso.
 
  if(2 == 4 || 3 >= 5 || 'a' == 'b'){
     document.write('Verdade');
@@ -10,6 +11,14 @@ else{
 }
 ele vai retornar falso porque todas as condições são falsas
 
+=========================NEGAÇÃO ===========================
+if(!(2 == 2)){
+    document.write('Verdade');
+}
+else{
+    document.write('Falso');
+}
+
 */
 if(2 == 2 || 3 >= 1 && 'a' == 'b'){
     document.write('Verdade');
@@ -17,3 +26,4 @@ if(2 == 2 || 3 >= 1 && 'a' == 'b'){
 else{
     document.write('Falso');
 }
+
