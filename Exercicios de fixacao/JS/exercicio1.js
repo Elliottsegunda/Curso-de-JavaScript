8d2bcf0acd33e96a51a9a462743bcd05df56802f
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-06 13:57:34 +0100

    Update exercicio1.js

---
+++ Exercicios de fixacao/JS/exercicio1.js
@@ -0,0 +1,9 @@
+
+var idade = prompt('Digite a sua idade');
+
+idade = parseInt(idade);
+
+if( idade >= 0 && idade <= 15){
+    document.write('Caro usuario, pelos criterios de idade o senhor é uma criança')
+
+}
