c96e35a745faa4e7b3191716c7adbdd317017c5b
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-06 14:10:58 +0100

    Update exercicio1.js

--- Exercicios de fixacao/JS/exercicio1.js
+++ Exercicios de fixacao/JS/exercicio1.js
@@ -13,9 +13,12 @@ else if( idade >= 15 && idade <= 30){
 else if( idade >= 30 && idade <= 60){
     document.write('Caro usuario, pelos criterios de idade o senhor é Adulto');
 }
-else if( idade >= 60){
+else if( idade >= 60 && idade <= 104){
     document.write('Caro usuario, pelos criterios de idade o senhor é Idoso');
 }
-else if( idade <= -1 && idade >= 108){
+else if(idade >= 108){
     document.write('Caro usuario, pelos criterios de idade o senhor está morto');
+}
+else{
+    document.write('idade não identificada')
 }
