57d5b436fb896ca89ae6095035ba4ac9875829cf
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-08 14:27:41 +0100

    Update exercicio2.js

--- Exercicios de fixacao/JS/exercicio2.js
+++ Exercicios de fixacao/JS/exercicio2.js
@@ -2,14 +2,13 @@
 var nome = prompt('Digite o seu nome:');
 var altura = prompt('Digite a sua altura em centimetro:');
 var peso = prompt('Digite o seu peso:');
-var massa =null;
 
  altura = parseFloat(altura);
  peso = parseFloat(peso);
 
  altura /=100;
 
- massa = peso / altura**2;
+ var massa = peso / altura**2;
 
   if (massa <16) {
     document.write(nome+' possui índice de massa corporal igual a: '+massa+' sendo classificado como : Baixo peso muito grave');
