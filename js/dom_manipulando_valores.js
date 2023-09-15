a4e01a172c1863ee7bb0b751b212f5067eda6e5a
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-15 11:26:30 +0100

    Update dom_manipulando_valores.js

--- js/dom_manipulando_valores.js
+++ js/dom_manipulando_valores.js
@@ -1,5 +1,24 @@
 
 function distribuirCaracter(){
     //passo 1: Selecionar o valor Digitado
-    document.getElementById('caracter');
+    var caracter_digitado = document.getElementById('caracter').value;
+    //limpar o campo de digitação
+    document.getElementById('caracter').value = '';
+
+    switch(caracter_digitado){
+        case '0' :
+        case '1' :
+        case '2' :
+        case '3' :
+        case '4' :
+        case '5' :
+        case '6' :
+        case '7' :
+        case '8' :
+        case '9' :
+            // todos os cases vieram sem break porque para qualquer um deles a acção será a mesma.
+        break;
+        default:
+            //adicionar no campo de letras, se não for um numero, é uma letra
+    }
 }
