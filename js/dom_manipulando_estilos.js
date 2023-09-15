c80530b964325e70e9190791e63cf99f8050a6d4
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-15 12:47:56 +0100

    Update dom_manipulando_estilos.js

--- js/dom_manipulando_estilos.js
+++ js/dom_manipulando_estilos.js
@@ -1,4 +1,6 @@
 
-function modificaEstilo(corDeFundo){
+function modificaEstilo(corDeFundo,l,a){
     document.getElementById('quadrado').style.background = corDeFundo;
+    document.getElementById('quadrado').style.width = l;
+    document.getElementById('quadrado').style.height = a;
 }
