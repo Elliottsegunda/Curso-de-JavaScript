1770746577a9e87ed10307a9d46f2f7d73bff5b8
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-15 11:06:19 +0100

    Update dom1.js

--- js/dom1.js
+++ js/dom1.js
@@ -1,1 +1,4 @@
-document.getElementById('nome_usuario')
+//console.log(document.getElementById('nome_usuario'));
+//console.log(document.getElementsByTagName('input'));
+//console.log(document.getElementsByClassName('campo_texto'));
+console.log(document.getElementsByName('nome'));
