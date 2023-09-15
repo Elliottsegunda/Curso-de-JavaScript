32c956314015add0c00883226f695dc0b61a4196
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-15 13:31:58 +0100

    Update exercicio4.js

--- Exercicios de fixacao/JS/exercicio4.js
+++ Exercicios de fixacao/JS/exercicio4.js
@@ -2,5 +2,14 @@ function focarInput(corDeFoco) {
     document.getElementById('texto').style.backgroundColor = corDeFoco;
 }
 function pegarValor(){
-   var num_caracter = document.getElementById('texto').value
+   var num_caracter = document.getElementById('texto').value;
+   
+   if( num_caracter.length >= 3 ){
+        document.getElementById('texto').style.backgroundColor =' #00ff00';
+   }
+   else{
+    document.getElementById('texto').style.backgroundColor =' #ff0000';
+
+   }
+   return num_caracter;
 }
