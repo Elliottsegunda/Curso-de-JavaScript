8dc2f496e8d99a8388fe010872dccf14de418a7b
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-18 10:33:02 +0100

    Update calculadora.js

--- Projectos/Calculadora/Js/calculadora.js
+++ Projectos/Calculadora/Js/calculadora.js
@@ -2,11 +2,14 @@ function calcular(tipo, valor){
     console.log(tipo, valor);
 
     if(tipo === 'acao'){
+        if(valor === 'c'){
+            //limpar o visor
+            document.getElementById('resultado').value = '';
+        }
 
     }
     else if(tipo === 'valor'){
-        var valor_campo = document.getElementById('resultado').value;
-        document.getElementById('resultado').value = valor_campo + valor;
+        document.getElementById('resultado').value += valor;
 
     }
 }
