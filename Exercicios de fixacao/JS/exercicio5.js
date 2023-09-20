0aed3f42b5418eae606cf60eb7b1447ffefbe907
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-20 13:52:55 +0100

    funcao adicionar

--- Exercicios de fixacao/JS/exercicio5.js
+++ Exercicios de fixacao/JS/exercicio5.js
@@ -1,4 +1,18 @@
 var objectos = Array();
 objectos[0] = 'Cadeira';
-objectos[0] = 'Impressora';
-objectos[0] = 'Garfo';
+objectos[1] = 'Impressora';
+objectos[2] = 'Garfo';
+
+function adicionarElemento(){
+    var dados_campo = document.getElementById('campo_texto').value;
+    if(dados_campo === ''){
+        alert('Exibir um valor valido');
+    }
+    else if(objectos.indexOf(dados_campo) === -1){
+        objectos.unshift(dados_campo);
+        console.log(objectos);
+        dados_campo.value = '';
+
+    }
+
+}
