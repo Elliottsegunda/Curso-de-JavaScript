ecf750509323878525b7247c877de2b5747c9223
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-20 14:03:50 +0100

    Update exercicio5.js

--- Exercicios de fixacao/JS/exercicio5.js
+++ Exercicios de fixacao/JS/exercicio5.js
@@ -6,13 +6,16 @@ objectos[2] = 'Garfo';
 function adicionarElemento(){
     var dados_campo = document.getElementById('campo_texto').value;
     if(dados_campo === ''){
-        alert('Exibir um valor valido');
+        alert('Informe um valor valido');
     }
     else if(objectos.indexOf(dados_campo) === -1){
         objectos.unshift(dados_campo);
         console.log(objectos);
-        dados_campo.value = '';
+       document.getElementById('campo_texto').value = '';
 
+    }
+    else{
+        alert('Objecto já adicionado');
     }
 
 }
