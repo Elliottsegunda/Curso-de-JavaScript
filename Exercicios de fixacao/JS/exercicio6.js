63214948b85255a9cd5202935daf25f2bb74f417
Elliott Segunda <eliothsegunda@gmail.com>
2023-09-26 15:39:08 +0100

    Update exercicio6.js

---
+++ Exercicios de fixacao/JS/exercicio6.js
@@ -0,0 +1,10 @@
+
+lista_pessoas = Array();
+lista_pessoas[0] = 'Elliott';
+lista_pessoas[1] = 'Davis';
+lista_pessoas[2] = 'Ambriz';
+lista_pessoas[3] = 'Segunda';
+
+    for(var i; i<= lista_pessoas.lenght;i++){
+        console.log(lista_pessoas[i]);
+    }
